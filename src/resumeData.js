5f113d506121ea36eb7bfc4c875ddbe49851df45
Mehdi Rahal <mehdi@lablabee.com>
2025-10-23 16:57:16 +0100

    Update resumeData.js for consistency in string quotes and add new portfolio item

--- src/resumeData.js
+++ src/resumeData.js
@@ -1,181 +1,181 @@
 let resumeData = {
-  imagebaseurl: "https://www.linkedin.com/in/mehdi-rahal-abdelaziz/",
-  name: "Mehdi Rahal",
-  role: "React Frontend Developer",
-  linkedinId: " https://www.linkedin.com/in/mehdi-rahal-abdelaziz/",
-  resumeLink:
-    "https://drive.google.com/file/d/1ZNafp0s0tDSWZmk3YxdXbDl7if_Tjjdl/view?usp=sharing",
-  skypeid: "Your skypeid",
+  imagebaseurl: 'https://www.linkedin.com/in/mehdi-rahal-abdelaziz/',
+  name: 'Mehdi Rahal',
+  role: 'React Frontend Developer',
+  linkedinId: ' https://www.linkedin.com/in/mehdi-rahal-abdelaziz/',
+  resumeLink: 'https://drive.google.com/file/d/1ZNafp0s0tDSWZmk3YxdXbDl7if_Tjjdl/view?usp=sharing',
+  skypeid: 'Your skypeid',
   roleDescription:
     " Ready to bring your vision to life. Check out my projects and experience. If you're ready to collaborate, reach out!",
   socialLinks: [
     {
-      name: "linkedin",
-      url: "https://www.linkedin.com/in/mehdi-rahal-abdelaziz/",
-      className: "fa fa-linkedin",
+      name: 'linkedin',
+      url: 'https://www.linkedin.com/in/mehdi-rahal-abdelaziz/',
+      className: 'fa fa-linkedin',
     },
     {
-      name: "github",
-      url: "https://github.com/Mehdi-Rh",
-      className: "fa fa-github",
+      name: 'github',
+      url: 'https://github.com/Mehdi-Rh',
+      className: 'fa fa-github',
     },
     {
-      name: "skype",
-      url: "https://twitter.com/MRahal92",
-      className: "fa fa-twitter",
+      name: 'skype',
+      url: 'https://twitter.com/MRahal92',
+      className: 'fa fa-twitter',
     },
   ],
   aboutme:
-    "Experienced Front-End Developer specializing in React. Proficient in translating Figma designs into interactive web solutions using React and MUI. Skilled in Redux, HTML5/CSS3, React Router, and React Hooks. Adept at optimizing code, creating reusable components, and implementing efficient React design patterns. Agile and Git proficient, promoting effective teamwork and code collaboration. Thrives on tackling complex state management and performance optimization challenges, consistently delivering innovative solutions through a collaborative approach.",
-  address: "Algeria",
-  website: "https://mehdi-rahal-az.netlify.app/", // Todo
+    'Experienced Front-End Developer specializing in React. Proficient in translating Figma designs into interactive web solutions using React and MUI. Skilled in Redux, HTML5/CSS3, React Router, and React Hooks. Adept at optimizing code, creating reusable components, and implementing efficient React design patterns. Agile and Git proficient, promoting effective teamwork and code collaboration. Thrives on tackling complex state management and performance optimization challenges, consistently delivering innovative solutions through a collaborative approach.',
+  address: 'Algeria',
+  website: 'https://mehdi-rahal-az.netlify.app/', // Todo
   education: [
     {
-      UniversityName: "Microverse Bootcamp",
-      specialization: "Full Stack Development",
-      MonthOfPassing: "October",
-      YearOfPassing: "2022",
+      UniversityName: 'Microverse Bootcamp',
+      specialization: 'Full Stack Development',
+      MonthOfPassing: 'October',
+      YearOfPassing: '2022',
       Achievements: [
-        "Spent 1300+ hours mastering algorithms, data structures, and front-end development while simultaneously developing projects with JavaScript, React, and Redux.",
-        "Developed skills in remote pair-programming using GitHub, industry-standard git-flow, and daily standups to communicate and collaborate with international remote developers. ",
+        'Spent 1300+ hours mastering algorithms, data structures, and front-end development while simultaneously developing projects with JavaScript, React, and Redux.',
+        'Developed skills in remote pair-programming using GitHub, industry-standard git-flow, and daily standups to communicate and collaborate with international remote developers. ',
       ],
     },
     {
-      UniversityName:
-        "University of Science and Technology of Oran - Mohamed Boudiaf",
-      specialization: "Master Degree in Electronic Engineering",
-      MonthOfPassing: "Sep",
-      YearOfPassing: "2015",
-      Achievements: ["Deployed a VoIP project: Commissioning the SIP protocol"],
+      UniversityName: 'University of Science and Technology of Oran - Mohamed Boudiaf',
+      specialization: 'Master Degree in Electronic Engineering',
+      MonthOfPassing: 'Sep',
+      YearOfPassing: '2015',
+      Achievements: ['Deployed a VoIP project: Commissioning the SIP protocol'],
     },
   ],
   work: [
     {
-      CompanyName: "Lablabee",
-      specialization: "React Frontend Developer",
-      MonthOfLeaving: "Present",
-      YearOfLeaving: "",
+      CompanyName: 'Lablabee',
+      specialization: 'React Frontend Developer',
+      MonthOfLeaving: 'Present',
+      YearOfLeaving: '',
       Achievements: [
-        "SPA Development: Contributed to the development of a Telco Cloud training web app using React, Redux, and Material UI.",
-        "Enhanced User Experience: Optimized and refactored the Web App and elevated its user satisfaction and functionality.",
-        "Code Refactoring Expertise: Led strategic code refactoring initiatives to enhance maintainability and adhere to best practices, reducing technical debt.",
-        "Modular Components: Created reusable components using React design patterns, bolstering modularity and agility.",
-        "Agile Collaboration: Powered teamwork through Slack and version control with GitLab for efficient project execution and issue resolution.",
-        "Precise Task Management: Managed tasks effectively in Jira, ensuring on-time deliverables aligned with project objectives.",
-        "Code Excellence: Translated complex designs into high-quality code, maintaining design integrity.",
-        "Proactive Technical Debt Management: Addressed technical debt proactively, elevating code quality and resilience.",
-        "Comprehensive Code Reviews: Led thorough code reviews for uniformity and minimized post-release issues.",
-        "Quality Assurance: Ensured high-quality releases through exhaustive UI and functionality testing.",
-        "Deadline-Driven: Consistently met tight deadlines, contributing to project success and client satisfaction.",
+        'SPA Development: Contributed to the development of a Telco Cloud training web app using React, Redux, and Material UI.',
+        'Enhanced User Experience: Optimized and refactored the Web App and elevated its user satisfaction and functionality.',
+        'Code Refactoring Expertise: Led strategic code refactoring initiatives to enhance maintainability and adhere to best practices, reducing technical debt.',
+        'Modular Components: Created reusable components using React design patterns, bolstering modularity and agility.',
+        'Agile Collaboration: Powered teamwork through Slack and version control with GitLab for efficient project execution and issue resolution.',
+        'Precise Task Management: Managed tasks effectively in Jira, ensuring on-time deliverables aligned with project objectives.',
+        'Code Excellence: Translated complex designs into high-quality code, maintaining design integrity.',
+        'Proactive Technical Debt Management: Addressed technical debt proactively, elevating code quality and resilience.',
+        'Comprehensive Code Reviews: Led thorough code reviews for uniformity and minimized post-release issues.',
+        'Quality Assurance: Ensured high-quality releases through exhaustive UI and functionality testing.',
+        'Deadline-Driven: Consistently met tight deadlines, contributing to project success and client satisfaction.',
       ],
     },
     {
-      CompanyName: "Microverse",
-      specialization: "Front End Developer",
-      MonthOfLeaving: "Oct",
-      YearOfLeaving: "2021",
+      CompanyName: 'Microverse',
+      specialization: 'Front End Developer',
+      MonthOfLeaving: 'Oct',
+      YearOfLeaving: '2021',
       Achievements: [
-        "Spent 1300+ hours mastering algorithms, data structures, and front-end development while simultaneously developing projects with JavaScript and React.",
-        "Mastered intermediate and advanced topics in front-end development (including React, Git, and DS&A challenges) while building multiple front-end projects independently and via pair programming",
-        "Developed skills in remote pair-programming using GitHub, industry-standard gitflow, and daily standups to communicate and collaborate with international remote developers. ",
-        "Mentored junior web developers, providing technical support through code reviews.",
+        'Spent 1300+ hours mastering algorithms, data structures, and front-end development while simultaneously developing projects with JavaScript and React.',
+        'Mastered intermediate and advanced topics in front-end development (including React, Git, and DS&A challenges) while building multiple front-end projects independently and via pair programming',
+        'Developed skills in remote pair-programming using GitHub, industry-standard gitflow, and daily standups to communicate and collaborate with international remote developers. ',
+        'Mentored junior web developers, providing technical support through code reviews.',
       ],
     },
   ],
-  skillsDescription: "Your skills here",
+  skillsDescription: 'Your skills here',
   skills: {
     languages: [
       {
-        skillname: "HTML5",
-        image: "images/skills/html-5.svg",
+        skillname: 'HTML5',
+        image: 'images/skills/html-5.svg',
       },
       {
-        skillname: "CSS",
-        image: "images/skills/css.svg",
+        skillname: 'CSS',
+        image: 'images/skills/css.svg',
       },
       {
-        skillname: "Javascript",
-        image: "images/skills/javascript.svg",
+        skillname: 'Javascript',
+        image: 'images/skills/javascript.svg',
       },
       {
-        skillname: "Typescript",
-        image: "images/skills/typescript.svg",
+        skillname: 'Typescript',
+        image: 'images/skills/typescript.svg',
       },
     ],
-    "tools-frameworks": [
+    'tools-frameworks': [
       {
-        skillname: "Reactjs",
-        image: "images/skills/react js.svg",
+        skillname: 'Reactjs',
+        image: 'images/skills/react js.svg',
       },
 
       {
-        skillname: "Redux",
-        image: "images/skills/redux.svg",
+        skillname: 'Redux',
+        image: 'images/skills/redux.svg',
       },
       {
-        skillname: "Material-UI",
-        image: "images/skills/material-ui.svg",
+        skillname: 'Material-UI',
+        image: 'images/skills/material-ui.svg',
       },
       {
-        skillname: "Tailwind",
-        image: "images/skills/tailwind.svg",
+        skillname: 'Tailwind',
+        image: 'images/skills/tailwind.svg',
       },
       {
-        skillname: "Bootstrap",
-        image: "images/skills/bootstrap.svg",
+        skillname: 'Bootstrap',
+        image: 'images/skills/bootstrap.svg',
       },
       {
-        skillname: "Git",
-        image: "images/skills/git.svg",
+        skillname: 'Git',
+        image: 'images/skills/git.svg',
       },
       {
-        skillname: "VsCode",
-        image: "images/skills/vscode.svg",
+        skillname: 'VsCode',
+        image: 'images/skills/vscode.svg',
       },
     ],
   },
   portfolio: [
-  {
-    "name": "LabLabee",
-    "description": "B2B Telco Cloud training platform",
-    "imgurl": "images/myImages/screenshots/Portfolio-1.png",
-    "demoUrl": "https://www.youtube.com/watch?v=jEniF78rD7E&t=226s&ab_channel=ApisTraining",
-    "skills": ["Javascript", "React", "Redux", "Material-UI"]
-  },
-  {
-    "name": "Bookit Mern",
-    "description": "This is a learning portfolio project where I converted the Booking App built by Brad (owner of Traversy Media) from NextJS to a MERN-based project to sharpen my skills in this stack. Guest credentials: email: guest@example.com password: guest@example.com2025CAP",
-    "imgurl": "images/myImages/screenshots/Bookit.png",
-    "demoUrl": "https://bookit-mern-c3pj.onrender.com/",
-    "sourceUrl": "https://github.com/Mehdi-Rh/Bookit-Mern",
-    "skills": [
-      "Javascript",
-      "React",
-      "Express",
-      "MongoDb",
-      "Cloudinary",
-      "React-Router",
-      "Tailwind"
-    ]
-  },
-  {
-    "name": "Algerian Summit",
-    "description": "This project is a conference responsive website built to showcase the activities of an event, buils with HTML&CSS and Javascript.",
-    "imgurl": "images/myImages/screenshots/Portfolio-3.png",
-    "liveUrl": "https://mehdi-rh.github.io/Blog-Capstone",
-    "sourceUrl": "https://github.com/Mehdi-Rh/Blog-Capstone",
-    "skills": ["Javascript", "HTML", "CSS"]
-  }
-]
-,
-  testimonials: [
     {
-      description: "This is a sample testimonial",
-      name: "Some technical guy",
+      name: 'LabLabee',
+      description: 'B2B Telco Cloud training platform',
+      imgurl: 'images/myImages/screenshots/Portfolio-1.png',
+      demoUrl: 'https://www.youtube.com/watch?v=jEniF78rD7E&t=226s&ab_channel=ApisTraining',
+      skills: ['Javascript', 'React', 'Redux', 'Material-UI'],
+    },
+
+    {
+      name: 'AI Research Explorer',
+      description:
+        'AI Research Explorer - a web application that allows users to search, analyze, and interact with research content using AI capabilities.',
+      imgurl: 'images/myImages/screenshots/Portfolio-3.png',
+      liveUrl: 'https://ai-research-explorer-psi.vercel.app/',
+      sourceUrl: 'https://github.com/Mehdi-Rh/AI-Research-Explorer?tab=readme-ov-file',
+      skills: ['React', 'Typescript', 'Material UI', 'Cohere AI'],
     },
     {
-      description: "This is a sample testimonial",
-      name: "Some technical guy",
+      name: 'Bookit Mern',
+      description:
+        'This is a learning portfolio project where I converted the Booking App built by Brad (owner of Traversy Media) from NextJS to a MERN-based project to sharpen my skills in this stack. Guest credentials: email: guest@example.com password: guest@example.com2025CAP',
+      imgurl: 'images/myImages/screenshots/Bookit.png',
+      demoUrl: 'https://bookit-mern-c3pj.onrender.com/',
+      sourceUrl: 'https://github.com/Mehdi-Rh/Bookit-Mern',
+      skills: [
+        'Javascript',
+        'React',
+        'Express',
+        'MongoDb',
+        'Cloudinary',
+        'React-Router',
+        'Tailwind',
+      ],
+    },
+  ],
+  testimonials: [
+    {
+      description: 'This is a sample testimonial',
+      name: 'Some technical guy',
+    },
+    {
+      description: 'This is a sample testimonial',
+      name: 'Some technical guy',
     },
   ],
 };
